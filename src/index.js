20c6e41bd6215a57678e1e0b16bd46e6b9098afc
hafsadahir <hafsa.dahir@student.moringaschool.com>
2022-08-31 18:51:23 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  let formElement = document.querySelector("form")
+  formElement.addEventListener("submit",(e) => {
+    e.preventDefault()
+    listOfTask(e.target.new_task_description.value)
+    formElement.reset()
+  })
+  
+  function listOfTask(task) {
+    let p = documentcreateElement("p")
+    p.textContent = `${task}`
+    let btn = document.createElement("button")
+    btn.addEventListener("click",deleteList)
+    btn.textContent = " x "
+    p.appendChild(btn)
+
+  document.getElementById("list").appendChild(p)
+  
+}
+
+function deleteList(e) {
+  e.target.parentNode.remove()
+
+}
+
 });
